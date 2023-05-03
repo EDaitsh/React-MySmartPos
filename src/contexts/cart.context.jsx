9020e7358d721ed481ce87bdbb1af6a4b81086ac
edaitsh <e.daitsh@gmail.com>
2023-05-03 22:33:10 +0300

    CartContext and LoyaltyCotext useReducer instead of useState

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,8 +1,8 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useReducer} from "react";
 import { addItemReq,
          voidItemReq
        } from "../utils/fast-lane-bridge-webapi/fast-lane-bridge-webapi.utils";
-
+import { createAction } from "../utils/reducer/reducer.utils";
 
 const addCartItem = (cartItems, productToAdd) => {
     console.log(productToAdd);
@@ -38,54 +38,84 @@ export const CartContext = createContext({
     cartItems: [],
     cartCount: 0,
     cartTotal: 0,
-    setCartTotal: () => {},
     initialCartState: ()=> {},
     addItemToCart: () => {},
     removeItemFromCart: () => {},
     clearItemFromCart: () => {}
 });
 
+const CART_ACTION_TYPES = {
+    SET_CART_ITEMS: 'SET_CART_ITEMS',
+}
+
+const CartReducer = (state, action) => {
+    const {type, payload} = action;
+    switch (type) {
+        case CART_ACTION_TYPES.SET_CART_ITEMS:
+            return {
+                ...state,
+                ...payload
+            }
+        default:
+            throw new Error(`Unhandled type ${type} in CartReducer`);
+    }
+}
+
+const INITIAL_STATE = {
+    cartItems: [],
+    cartCount: 0,
+    cartTotal: 0,
+}
+
 export const CartProvider=({children}) => {
-    const [cartItems, setCartItems] = useState([]);
-    const [cartCount, setCartCount] = useState(0);
-    const [cartTotal, setCartTotal] = useState(0);
+    const [state, dispatcher] = useReducer(CartReducer, INITIAL_STATE);
 
+    const {cartItems, cartCount, cartTotal} = state;
 
-    useEffect (()=> 
-    {
-        setCartCount(cartItems.reduce((count, item) => count + item.quantity, 0));
-        setCartTotal(cartItems.reduce((total, item) => 
-            total +parseFloat((item.quantity * item.price).toFixed(2))
-        ,0));
-    }, [cartItems]);
+    const updateCartItemsReducer= (newCartItems) => {
+       
+        const newCartCount = newCartItems.reduce((acumelator, currentValue) => acumelator + parseInt(currentValue.quantity), 0)
+        const newCartTotal = newCartItems.reduce((total, item) => total +(item.quantity * item.price),0)
+        dispatcher(createAction(CART_ACTION_TYPES.SET_CART_ITEMS,{
+                cartItems: newCartItems,
+                cartCount: newCartCount,
+                cartTotal: newCartTotal
+                }
+        ));
+    }
+
 
     const initialCartState =() => {
-        setCartItems([]);
-      }
+        const newCartItems = [];
+        updateCartItemsReducer(newCartItems);
+    }
 
      const addItemToCart= async(upc, quantity = 1) => {
         const res = await addItemReq(upc, quantity);
-        setCartItems(addCartItem(cartItems, res["ItemSold"]));
+        const newCartItems = addCartItem(cartItems, res["ItemSold"]);
+        updateCartItemsReducer(newCartItems);
     }
 
     const removeItemFromCart= async(cartItemToRemove) => {
         const {upc} = cartItemToRemove;
         const res = await voidItemReq(upc, 1);
-        setCartItems(removeCartItem(cartItems, cartItemToRemove));
+        const newCartItems  = removeCartItem(cartItems, cartItemToRemove);
+        updateCartItemsReducer(newCartItems);
     }
 
     const clearItemFromCart = async(cartItemToRemove) => {
         const {upc, quantity} = cartItemToRemove;
         const res = await voidItemReq(upc, quantity);
-        setCartItems(clearCartItem(cartItems, cartItemToRemove));
+        const newCartItems = clearCartItem(cartItems, cartItemToRemove);
+        updateCartItemsReducer(newCartItems);
     }
+
 
 
     const value = {
         cartItems,
         cartCount,
         cartTotal,
-        setCartTotal,
         initialCartState,
         addItemToCart,
         removeItemFromCart,
--- src/contexts/loyalty.context.jsx
+++ src/contexts/loyalty.context.jsx
@@ -1,12 +1,38 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer} from "react";
+import { createAction } from "../utils/reducer/reducer.utils";
 
 export const LoyaltyContext = createContext({
     loyaltyNumber: '',
     setLoyaltyNumber : () => {}
 });
 
+const LOYALTY_ACTION_TYPE = {
+    SET_LOYALTY_NUMBER: 'SET_LOYALTY_NUMBER'
+}
+
+const INITIAL_STATE = {
+    loyaltyNumber: ''
+}
+
+const LoyaltyReducer = (state, action) => {
+    const {type, payload} = action;
+    switch (type) {
+        case LOYALTY_ACTION_TYPE.SET_LOYALTY_NUMBER:
+            return {
+                ...state,
+                loyaltyNumber: payload
+            }
+        default:
+            throw new Error(`Unhandled type ${type} in LoyaltyReducer`);
+    }
+}
+
 export const LoyaltyProvider = ({children}) => {
-    const [loyaltyNumber, setLoyaltyNumber] = useState('');
+    // const [loyaltyNumber, setLoyaltyNumber] = useState('');
+    const [{loyaltyNumber}, dispatcher] = useReducer(LoyaltyReducer, INITIAL_STATE);
+    const setLoyaltyNumber = (loyaltyNumber) => {
+        dispatcher(createAction(LOYALTY_ACTION_TYPE.SET_LOYALTY_NUMBER, loyaltyNumber));
+    }
 
     const value = {
         loyaltyNumber,
